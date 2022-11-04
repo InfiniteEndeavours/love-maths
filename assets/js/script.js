0bad9b7b9abe1de1f89a6726b71e5c3c66ecc50d
InfinteEndeavours <ethan.pitt@protonmail.com>
2022-11-04 19:03:33 +0000

    Rework division game to display whole number as a result instead of decimal

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === 'subtract') {
         displaySubtractQuestion(num1, num2);
-    } else if (gameType === 'division') {
+    } else if (gameType === 'division'){
         displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown Game Type: ${gameType}`);
@@ -90,9 +90,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === '-') {
         return [operand1 - operand2, "subtract"];
-    } else if (operator === '/') {
-        return [Math.round(operand1 / operand2 * 100) / 100, "division"];
-    } else {
+    } else if (operator === '/'){
+        return [operand1 % operand2, "division"];
+    }else {
         alert(`Unimplemented operator ${operator}`);
         throw (`Unimplemented operator ${operator}. Aborting!`);
     }
@@ -141,6 +141,4 @@ function displayDivisionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
-    console.log("Operand 1: " + operand1)
-    console.log("Operant 2: " + operand2)
 }
